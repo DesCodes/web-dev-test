f6c8f51768774c901cc2053891940a48cda0bfeb
Desmond <desmond.yan.lai@gmail.com>
2020-09-03 14:13:28 -0400

    fixed draggable feature

--- main.js
+++ main.js
@@ -1,11 +1,18 @@
 $(document).ready(function(){
+	// gives draggable funtion to the product img
+	$('.productContainer figure img').draggable({
+		axis: 'x', 
+		containment: '.wrapper',
+		revert: true,
+	})
+
+
+			// animation for right arrow click and section click, circle2 (window width < 1350px)
 	$('.backgroundContainer section, .arrowRight, .circle2').on('click', function(){
 		function checkWidth() {
 		var windowSize = $(window).width();
 
 		if(windowSize <= 1350) {
-			console.log('1350')
-			// animation for right arrow click and section click, circle2 (window width < 1350px)
 			$('.productTitleContainer').animate({top: '100%'}, 'slow')
 			$('.learnMore').hide()
 			$('.descriptionContainer p, .XMFitness').show('swing')
@@ -14,6 +21,7 @@ $(document).ready(function(){
 			$('.interactiveSlider .circle1').css('color', '#616161')
 			$('.interactiveSlider .circle2').css('color', '#bfab7c')
 			$('html, body').stop().animate({scrollTop: $('.descriptionContainer').offset().top}, 2000, 'swing')
+		
 		} else {
 			// animation for right arrow click, section click, circle2
 			$('.productContainer').animate({left: '38%'}, 'slow')
@@ -24,11 +32,12 @@ $(document).ready(function(){
 			$('.learnMore').hide()
 			$('.interactiveSlider .circle1').css('color', '#616161')
 			$('.interactiveSlider .circle2').css('color', '#bfab7c')
-			
+			$('.productContainer figure img').draggable('disable')
 		}
 	}
 	checkWidth();
 	});
+
 
 	$('.arrowLeft, .circle1').on('click', function(){
 		function checkWidth() {
@@ -44,6 +53,8 @@ $(document).ready(function(){
 				$('.interactiveSlider .circle2').css('color', '#616161')
 				$('.interactiveSlider .circle1').css('color', '#bfab7c')
 				$('html, body').stop().animate({scrollTop: $('.productContainer').offset().top}, 2000, 'swing')
+				$('.productContainer figure img').draggable('enable')
+
 			} else {
 				// animation for left arrow click and circke1 click
 				$('.learnMore').show('swing')
@@ -58,26 +69,30 @@ $(document).ready(function(){
 		checkWidth();
 	});
 
-	$('.productContainer figure img').draggable({
-		axis: 'x', 
-		containment: '.wrapper',
-		revert: true,
-	})
-	// fade to left, reapear on the right side and fade in
-	// on drag, auto fade away when img reaches left: -350px
 
-	// $('.productContainer figure img').draggable({'disable'})
-	// $('.productContainer figure img').draggable({'enable'})
 
-	$('.productContainer figure img').on('mousedown ontouchmove', function() {
-			timeOut = setTimeout(function checkImgPosition() {
-				var position = $('.productContainer figure img').position()
-				// console.log(position.left)
+	// img draging function
+	$('.productContainer figure img').on('mousedown', function() {
+		timeOut = setTimeout(function() {
+		var position = $('.productContainer figure img').position();	
+			
+			if(position.left < 0) {
 
-				if(position.left < -340) {
+				var windowSize = $(window).width();
+				if(windowSize <= 1350) {
+						$('.productTitleContainer').animate({top: '100%'}, 'slow')
+						$('.learnMore').hide()
+						$('.descriptionContainer p, .XMFitness').show('swing')
+						$('.descriptionContainer p').animate({opacity: '1'}, 'slow')
+						$('.descriptionContainer').css('width', '100%')
+						$('.interactiveSlider .circle1').css('color', '#616161')
+						$('.interactiveSlider .circle2').css('color', '#bfab7c')
+						$('html, body').stop().animate({scrollTop: $('.descriptionContainer').offset().top}, 2000, 'swing')
+						$('.productContainer figure img').draggable('disable')
+				} else {
+
 					console.log(position.left)
-					$('.productContainer figure img').fadeOut('fast')
-					// $('.productContainer figure img').css('left', '38%')
+					$('.productContainer figure img').fadeOut('fast').fadeIn('slow').draggable('disable')
 					$('.productContainer').animate({left: '38%'}, 'fast')
 					$('.productTitleContainer').animate({top: '23%'}, 'slow')
 					$('.descriptionContainer p, .XMFitness').show('swing')
@@ -86,20 +101,10 @@ $(document).ready(function(){
 					$('.learnMore').hide()
 					$('.interactiveSlider .circle1').css('color', '#616161')
 					$('.interactiveSlider .circle2').css('color', '#bfab7c')
-					$('.productContainer figure img').fadeIn('slow')
-					$('.productContainer figure img').draggable('disable')
 				}
-			}, 200).on('mouseup', function() {
-				clearTimeout(timeOut)
-			})
-
-			// function checkImgPosition() {
-			// 	var position = $('.productContainer figure img').position()
-			// 	console.log(position.left)
-			// }
-	// checkImgPosition();
+			}
+		}, 200)
 	})
-
 
 
 
